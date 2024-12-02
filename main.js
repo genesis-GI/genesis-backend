a23e4c9078006143ab531adecd7067ff56d24f4a
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2024-12-02 20:50:03 +0100

    Added Design

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 8080
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
