d9316a1bcfe8e5e6765e3ebdd21e5677005496a7
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2024-12-27 23:19:16 +0100

    spectrum hotfix

--- spectrum.js
+++ spectrum.js
@@ -5,42 +5,44 @@ function formatTime(seconds) {
         return 'a few seconds ago';
     }
     const minutes = Math.floor(seconds / 60);
-    if(minutes == 1){
+    if (minutes == 1) {
         return `${minutes} minute ago`;
-    }else if (minutes < 60) {
+    } else if (minutes < 60) {
         return `${minutes} minutes ago`;
     }
 
     const hours = Math.floor(minutes / 60);
-    if(hours == 1){
+    if (hours == 1) {
         return `${hours} hour ago`;
-    }else if (hours < 24) {
+    } else if (hours < 24) {
         return `${hours} hours ago`;
     }
 
     const days = Math.floor(hours / 24);
-    if(days == 1){
+    if (days == 1) {
         return `${days} day ago`;
-    }else if (days < 7) {
+    } else if (days < 7) {
         return `${days} days ago`;
     }
 
     const weeks = Math.floor(days / 7);
-    if(weeks == 1){
+    if (weeks == 1) {
         return `${weeks} week ago`;
-    }if (weeks < 4) {
+    } else if (weeks < 4) {
         return `${weeks} weeks ago`;
     }
 
     const months = Math.floor(days / 30);
-    if (months < 12) {
+    if (months == 1) {
+        return `${months} month ago`;
+    } else if (months < 12) {
         return `${months} months ago`;
     }
 
     const years = Math.floor(days / 365);
-    if(years == 1){
+    if (years == 1) {
         return `${years} year ago`;
-    }else {
+    } else {
         return `${years} years ago`;
     }
 }
@@ -52,7 +54,10 @@ function listenForMOTDUpdates(channel, callback) {
             const data = doc.data();
             const motdInfos = {
                 message: data.message,
-                lastChanged: formatTime((Date.now() - data.date.toDate().getTime()) / 1000)
+                date: {
+                    _seconds: data.date.seconds,
+                    _nanoseconds: data.date.nanoseconds
+                }
             };
             callback(motdInfos);
         }
