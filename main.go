79eb13c8bacf8d635a3608a878bbb54b32db9148
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-01-05 18:15:18 +0100

    Began go rewrite
    Rewrote all simple "get" functions, that don't need cookies or deeper database integration.

--- main.go
+++ main.go
@@ -1,22 +1,45 @@
 package main
 
-import(
-	"github.com/gin-gonic/gin"
+import (
+
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+	r.Static("/public/css", "./public/css")
 
+	r.GET("/css/styles.css", func(c *gin.Context) {
+		c.File("public/css/styles.css")
+	})
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
+		c.File("public/landing.html")
 	})
 
 
+	r.GET("/login", func(c *gin.Context) {
+		c.File("public/login.html")
+	})
+
+	r.GET("/register", func(c *gin.Context){
+		c.File("public/register.html")
+	})
+
+	r.GET("/download", func(c *gin.Context) {
+		c.File("public/launcherdownload.html")
+	})
+
+	r.GET("/spectrum", func(c *gin.Context) {
+		c.File("public/spectrum.html")
+	})
+
+	r.GET("/performance", func(c *gin.Context) {
+		c.File("public/Genesis-website-performance-report-2.html")
+	})
 
 	api := r.Group("/api")
 	{
@@ -27,6 +50,6 @@ func main() {
 		})
 	}
 
-	fmt.Println("Server is running on http://localhost:8088")
-	r.Run(":8088")
+	fmt.Println("Server is running on http://localhost:8089")
+	r.Run(":8089")
 }
